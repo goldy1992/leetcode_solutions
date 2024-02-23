2b55250b9f49e64af2e40478e1cf930844228790
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2024-02-23 02:50:24 +0000

    Added Solution to jump game II

---
+++ src/test/kotlin/com/goldy1992/leetcode/_45_jump_game_ii/SolutionTest.kt
@@ -0,0 +1,39 @@
+package com.goldy1992.leetcode._45_jump_game_ii
+
+import org.example.com.goldy1992.leetcode._45_jump_game_ii.Solution
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+
+    private val solution = Solution()
+    @Test
+    fun testExample1() {
+        val input = intArrayOf(2,3,1,1,4)
+        val expected = 2
+        val result = solution.jump(input)
+        assertEquals(expected, result)
+    }
+    @Test
+    fun testExample2() {
+        val input = intArrayOf(2,3,0,1,4)
+        val expected = 2
+        val result = solution.jump(input)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testExample3() {
+        val input = intArrayOf(2,3,1)
+        val expected = 1
+        val result = solution.jump(input)
+        assertEquals(expected, result)
+    }
+    @Test
+    fun testExample4() {
+        val input = intArrayOf(1,2,1,1,1)
+        val expected = 3
+        val result = solution.jump(input)
+        assertEquals(expected, result)
+    }
+}
