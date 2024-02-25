4da52b1f3599a9f5137161789248f1716404cfd4
Mike <goldy13_1992@hotmail.co.uk>
2024-02-25 21:15:40 +0700

    Latest RandomizedSet changes

--- src/main/kotlin/com/goldy1992/leetcode/_380_insert_delete_random_o1/RandomizedSet.kt
+++ src/main/kotlin/com/goldy1992/leetcode/_380_insert_delete_random_o1/RandomizedSet.kt
@@ -1,5 +1,7 @@
 package com.goldy1992.leetcode._380_insert_delete_random_o1
 
+import kotlin.random.Random
+
 class Struct(val num: Int) {
     var left: Struct? = null
     var right: Struct? = null
@@ -7,6 +9,7 @@ class Struct(val num: Int) {
 
 class Tree {
     var root : Struct? = null
+    var size = 0
 }
 
 
@@ -14,12 +17,14 @@ class RandomizedSet() {
 
     val tree : Tree = Tree()
 
+
     fun insert(`val`: Int): Boolean {
+        var foundSpot = false
         if (tree.root == null) {
             tree.root = Struct(`val`)
-            return true
+            foundSpot = true
         }
-        var foundSpot = false
+
         var currentStruct : Struct = tree.root!!
         while (!foundSpot) {
             val currentNum = currentStruct.num
@@ -39,16 +44,150 @@ class RandomizedSet() {
                 currentStruct = currentStruct.right!!
             }
         }
+        tree.size++
         return true
 
     }
 
     fun remove(`val`: Int): Boolean {
+        if (tree.root == null) {
+            return false
+        }
+
+        var foundSpot = false
+        var parent: Struct? = null
+        var parentLeft = false
+        var currentStruct : Struct = tree.root!!
+        while (!foundSpot) {
+            val currentNum = currentStruct.num
+            if (currentNum == `val`) {
+                foundSpot = true
+            } else if (`val` < currentNum) {
+                if (currentStruct.left != null) {
+                    parent = currentStruct
+                    parentLeft = true
+                    currentStruct = currentStruct.left!!
+                } else {
+                    return false
+                }
+            } else {
+                if (currentStruct.right != null) {
+                    parent = currentStruct
+                    parentLeft = false
+                    currentStruct = currentStruct.right!!
+                } else {
+                    return false
+                }
+            }
+        }
+
+        var numChildren = 0
+        if (currentStruct.left != null) {
+            numChildren++
+        }
+        if (currentStruct.right != null) {
+            numChildren++
+        }
+
+        if (numChildren == 0) {
+            if (parent != null) {
+                if (parentLeft) {
+                    parent.left = null
+                } else {
+                    parent.right = null
+                }
+            } else {
+                tree.root = null
+            }
+            return true
+        } else if (numChildren == 1) {
+            val replaceStruct = if (currentStruct.left != null)
+                currentStruct.left else currentStruct.right
+            if (parent != null) {
+                if (parentLeft) {
+                    parent.left = replaceStruct
+                } else {
+                    parent.right = replaceStruct
+                }
+            } else {
+                tree.root = replaceStruct
+            }
+        } else {
+            // find replacement Node
+            var foundReplacement= false
+            var replaceParent = currentStruct
+            var replaceParentLeft = false
+            var replaceStruct : Struct = currentStruct.left!!
+            while (!foundReplacement) {
+                if (replaceStruct.right != null) {
+                    replaceParent = replaceStruct
+                    replaceParentLeft = false
+
+                    replaceStruct = replaceStruct.right!!
+                } else if (replaceStruct.left != null) {
+                    replaceParent = replaceStruct
+                    replaceParentLeft = true
+                    replaceStruct = replaceStruct.left!!
+                } else {
+                    if (replaceParentLeft) {
+                        replaceParent.left = null
+                    }
+                    else {
+                        replaceParent.right = null
+                    }
+                    foundReplacement = true
+                }
+            }
+
+            if (parent != null) {
+                if (parentLeft) {
+                    parent.left = replaceStruct
+                } else {
+                    parent.right = replaceStruct
+                }
+            } else {
+                tree.root = replaceStruct
+            }
+        }
+
+
+        tree.size--
         return true
+
     }
 
+    data class Traversal(
+        val index: Int,
+        val value: Int
+    )
+    fun traverse(node: Struct, traversal: Traversal, targetIdx: Int) : Traversal {
+        if (traversal.index == targetIdx) {
+            return traversal
+        }
+        var newTraversal = traversal
+        if (node.left != null) {
+            newTraversal = traverse(node.left!!, newTraversal, targetIdx)
+            if (newTraversal.index == targetIdx) {
+                return newTraversal
+            }
+        }
+        newTraversal = Traversal(newTraversal.index + 1, node.num)
+        if (newTraversal.index == targetIdx) {
+            return newTraversal
+        }
+        if (node.right != null) {
+            newTraversal = traverse(node.right!!, newTraversal, targetIdx)
+        }
+
+        return newTraversal
+
+    }
     fun getRandom(): Int {
-        return 0
+        if (tree.size == 1) {
+            return tree.root!!.num
+        }
+        val toRetrieveIdx = Random.nextInt(1, tree.size)
+        return traverse(tree.root!!, Traversal(0, -1), toRetrieveIdx).value
     }
 
 }
--- src/test/kotlin/com/goldy1992/leetcode/_380_insert_delete_random_o1/RandomizedSetTest.kt
+++ src/test/kotlin/com/goldy1992/leetcode/_380_insert_delete_random_o1/RandomizedSetTest.kt
@@ -6,10 +6,68 @@ import kotlin.test.Test
 class RandomizedSetTest {
     private val set = RandomizedSet()
 
+    companion object {
+        const val CREATE = "RandomizedSet"
+        const val INSERT ="insert"
+        const val REMOVE = "remove"
+        const val RANDOM = "getRandom"
+    }
     @Test
     fun testExample1() {
         assertTrue(set.insert(0))
         assertTrue(set.insert(1))
         assertFalse(set.insert(1))
     }
+    @Test
+    fun testExample2() {
+        val commands = arrayOf("RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom")
+        val values = arrayOf<Int?>(null,1,2,2,null,1,2,null)
+        val expected = arrayOf<String?>(null,"true","false","true","1","true","false","2")
+        val result = performTest(commands, values)
+
+        assertArrayEquals(expected, result)
+    }
+    @Test
+    fun testExample3() {
+        val commands = arrayOf("RandomizedSet","insert","insert","insert","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom")
+        val values = arrayOf<Int?>(null,0,1,2,null,null,null,null,null,null,null,null,null,null)
+        //val expected = arrayOf<String?>(null,"true","false","true","1","true","false","2")
+        val result = performTest(commands, values)
+
+       // assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun testExample4() {
+        val commands = arrayOf("RandomizedSet","insert","insert","remove","insert","insert","insert","remove","remove","insert","remove","insert","insert","insert","insert","insert","getRandom","insert","remove","insert","insert")
+        val values = arrayOf<Int?>(null,3,-2,2,1,-3,-2,-2,3,-1,-3,1,-2,-2,-2,1,null,-2,0,-3,1)
+        val expected = arrayOf<String?>(null,"true","true","false","true","true","false","true","true","true","true","false","true","false","false","false","-2","false","false","true","false")
+        val result = performTest(commands, values)
+
+        assertArrayEquals(expected, result)
+    }
+
+
+
+    private fun performTest(commands: Array<String>, values : Array<Int?>) : Array<String?> {
+        val toReturn = arrayOfNulls<String?>(commands.size)
+        var rs: RandomizedSet? = null
+
+        for (i in commands.indices) {
+
+            val command = commands[i]
+            when(command) {
+                CREATE -> {
+                    rs = RandomizedSet()
+                    toReturn[i] = null
+                }
+                INSERT -> toReturn[i] = rs!!.insert(values[i]!!).toString()
+                REMOVE -> toReturn[i] = rs!!.remove(values[i]!!).toString()
+                RANDOM -> toReturn[i] = rs!!.getRandom().toString()
+            }
+        }
+        return toReturn
+
+    }
+
 }
