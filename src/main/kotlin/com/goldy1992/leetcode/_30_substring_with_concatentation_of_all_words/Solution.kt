b80bb85da1321d91f34a23d71a1841271a0eb4b2
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2024-03-15 03:03:18 +0000

    Inefficient Solution for substring concatenation with all substrings

--- src/main/kotlin/com/goldy1992/leetcode/_30_substring_with_concatentation_of_all_words/Solution.kt
+++ src/main/kotlin/com/goldy1992/leetcode/_30_substring_with_concatentation_of_all_words/Solution.kt
@@ -2,6 +2,41 @@ package com.goldy1992.leetcode._30_substring_with_concatentation_of_all_words
 
 class Solution {
     fun findSubstring(s: String, words: Array<String>): List<Int> {
-        return emptyList()
+        val wordLength = words[0].length
+        val subStringSizeSize = wordLength * words.size
+        val toReturn = mutableListOf<Int>()
+        if (subStringSizeSize > s.length) {
+            return toReturn
+        }
+        var startIdx = 0
+        while (startIdx + subStringSizeSize  <= s.length) {
+            var currentIdx = startIdx
+            val indicesUsed = mutableSetOf<Int>()
+            for (i in currentIdx until currentIdx + subStringSizeSize step(wordLength)) {
+                var subStringFound = false
+                val currentSubString = s.substring(i , i + wordLength)
+                wordLoop@ for (index in words.indices) {
+                    if (!indicesUsed.contains(index) ) {
+                        val word = words[index]
+                        if (word == currentSubString) {
+                            indicesUsed.add(index)
+                            subStringFound = true
+                            break@wordLoop
+                        }
+                    }
+                }
+                if (subStringFound) {
+                    currentIdx += wordLength
+                } else {
+                    break
+                }
+            }
+
+            if (indicesUsed.size == words.size) {
+                toReturn.add(startIdx)
+            }
+            startIdx++
+        }
+        return toReturn
     }
 }
---
+++ src/test/kotlin/com/goldy1992/leetcode/TestUtils.kt
@@ -0,0 +1,22 @@
+package com.goldy1992.leetcode
+
+import java.io.BufferedReader
+import java.io.File
+
+object TestUtils {
+    private const val INPUT_DELIMITER = ","
+    fun readFileAsString(filePath: String) : String {
+        val bufferedReader: BufferedReader = File(filePath).bufferedReader()
+        return bufferedReader.use { it.readText() }
+   }
+
+    fun readFileAsStringArray(filePath: String) : Array<String> {
+        val bufferedReader: BufferedReader = File(filePath).bufferedReader()
+        val string = bufferedReader.use { it.readText() }
+        return string.split(INPUT_DELIMITER).toTypedArray()
+    }
+
+    fun buildFilePath(dir: String, fileName: String) : String {
+        return dir + File.separator + fileName
+    }
+}
