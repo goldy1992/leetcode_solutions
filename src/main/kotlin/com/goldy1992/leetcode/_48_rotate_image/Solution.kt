ef4bba4956235f2af18a1f1fc4edf736423198f6
Mike <goldy13_1992@hotmail.co.uk>
2024-03-24 10:26:34 +0700

    Added placeholder for rotate image

---
+++ src/main/kotlin/com/goldy1992/leetcode/_48_rotate_image/Solution.kt
@@ -0,0 +1,7 @@
+package com.goldy1992.leetcode._48_rotate_image
+
+class Solution {
+    fun rotate(matrix: Array<IntArray>): Unit {
+
+    }
+}
--- src/main/kotlin/com/goldy1992/leetcode/_54_spiral_matrix/Solution.kt
+++ src/main/kotlin/com/goldy1992/leetcode/_54_spiral_matrix/Solution.kt
@@ -2,12 +2,29 @@ package com.goldy1992.leetcode._54_spiral_matrix
 
 import kotlin.math.max
 
+/**
+ * Example:
+ * 1, 2, 3, 4,
+ * 12,13,14, 5,
+ * 11,16,15, 6,
+ * 10, 9, 8, 7
+ *
+ * (0,0) -> 4 iter -> (0,3)
+ * R+1
+ * (1,3) -> 3 iter -> (3,3)
+ * C-1
+ * (3,2) -> 3 iter -> (3,0)
+ * R-1
+ * (2,0) -> 2 iter -> (1,0)
+ * C+1
+ * (1,1) -> 2 iter -> (1,2)
+ * R+1
+ * (2,2) -> 1 iter -> (2,2)
+ * C-1
+ * (2,1) -> 1 iter -> (2,1)
+ */
 class Solution {
 
-    class Cell(
-        var row : Int = 0,
-        var col : Int = 0
-    )
     fun spiralOrder(matrix: Array<IntArray>): List<Int> {
         val rowSize = matrix.size
         val colSize = matrix[0].size
@@ -16,72 +33,75 @@ class Solution {
         var currentIdx = 0
 
         var increasing = true
-        //var rowStartIdx = 0
-        //var colStartIdx = 0
-        var currentCell = Cell()
-        var iteration = 0
-        val rowIterations : (Int) -> Int = { iter -> rowSize - iter - 2 }
-        val colIterations : (Int) -> Int = { iter -> colSize - iter -1 }
+
+        var currentRow = 0
+        var currentCol = 0
+
+        var rowIterCount = matrix.size - 2
+        var colIterCount = matrix[0].size - 1
+
         breakWhile@ while (currentIdx < toReturn.size) {
             // loop row
             if (increasing) {
-                val colStartIdx = currentCell.col
-                val lastColIdx = colStartIdx + colIterations(iteration)
-                for ( c in colStartIdx .. lastColIdx) {
-                    toReturn[currentIdx] = matrix[currentCell.row][c]
+                val startCol = currentCol
+                val lastCol = startCol + colIterCount
+                for ( c in startCol .. lastCol) {
+                    toReturn[currentIdx] = matrix[currentRow][c]
                     currentIdx++
                     if (currentIdx >= toReturn.size) {
                         break@breakWhile
                     }
-                    if (currentCell.col < lastColIdx) {
-                        currentCell.col++
+                    if (currentCol < lastCol) {
+                        currentCol++
                     }
                 }
-                currentCell.row += 1
-                val rowStartIdx = currentCell.row
-
-                val lastRowIdx = rowStartIdx + rowIterations(iteration)
-                for (r in rowStartIdx .. lastRowIdx) {
-                    toReturn[currentIdx] = matrix[r][currentCell.col]
+                colIterCount--
+                currentRow += 1
+                val lastRowIdx = currentRow + rowIterCount
+                for (r in currentRow .. lastRowIdx) {
+                    toReturn[currentIdx] = matrix[r][currentCol]
                     currentIdx++
                     if (currentIdx >= toReturn.size) {
                         break@breakWhile
                     }
-                    if (currentCell.row < lastRowIdx  ) {
-                        currentCell.row++
+                    if (currentRow < lastRowIdx  ) {
+                        currentRow++
                     }
 
                 }
+                rowIterCount--
+                currentCol--
                 increasing = false
-                iteration++
             } else {
-                val colStartIdx = currentCell.col - 1
-                for ( c in colStartIdx downTo  colStartIdx - iteration -1 ) {
-                    toReturn[currentIdx] = matrix[currentCell.row][c]
+                val startCol = currentCol
+                val lastCol = startCol - colIterCount
+                for ( c in startCol downTo  lastCol) {
+                    toReturn[currentIdx] = matrix[currentRow][c]
                     currentIdx++
                     if (currentIdx >= toReturn.size) {
                         break@breakWhile
                     }
-                    if (currentCell.col >= colStartIdx - iteration) {
-                        currentCell.col--
+                    if (currentCol > lastCol) {
+                        currentCol--
                     }
                 }
-                val rowStartIdx = currentCell.row -1
-
-                for (r in rowStartIdx downTo  iteration ) {
-                    toReturn[currentIdx] = matrix[r][currentCell.col]
+                colIterCount--
+                currentRow -= 1
+                val lastRowIdx = currentRow - rowIterCount
+                for (r in currentRow downTo lastRowIdx) {
+                    toReturn[currentIdx] = matrix[r][currentCol]
                     currentIdx++
                     if (currentIdx >= toReturn.size) {
                         break@breakWhile
                     }
-                    if (currentCell.row > iteration) {
-                        currentCell.row--
+                    if (currentRow > lastRowIdx  ) {
+                        currentRow--
                     }
 
                 }
+                rowIterCount--
+                currentCol++
                 increasing = true
-                currentCell.col++
-                iteration++
             }
         }
         return toReturn.asList()
