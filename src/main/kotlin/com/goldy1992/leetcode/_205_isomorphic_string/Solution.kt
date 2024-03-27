328354cd425cbb5a9c87c54fb0221f7e4d741827
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2024-03-27 12:38:32 +0000

    Added placeholder for isomorphic string

---
+++ src/main/kotlin/com/goldy1992/leetcode/_205_isomorphic_string/Solution.kt
@@ -0,0 +1,7 @@
+package com.goldy1992.leetcode._205_isomorphic_string
+
+class Solution {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        return true
+    }
+}
---
+++ src/test/kotlin/com/goldy1992/leetcode/_205_isomorphic_string/SolutionTest.kt
@@ -0,0 +1,30 @@
+package com.goldy1992.leetcode._205_isomorphic_string
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun testExample1() {
+        val s = "egg"
+        val t = "add"
+        val result = solution.isIsomorphic(s, t)
+        assertTrue(result)
+    }
+    @Test
+    fun testExample2() {
+        val s = "foo"
+        val t = "bar"
+        val result = solution.isIsomorphic(s, t)
+        assertFalse(result)
+    }
+    @Test
+    fun testExample3() {
+        val s = "paper"
+        val t = "title"
+        val result = solution.isIsomorphic(s, t)
+        assertTrue(result)
+    }
+}
