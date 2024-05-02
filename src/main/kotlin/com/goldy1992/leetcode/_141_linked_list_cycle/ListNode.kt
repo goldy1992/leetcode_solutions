d253fe807ee6c57eb7e19fd4873e627749c78bb0
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2024-05-02 08:41:40 +0100

    Added placeholder for linked list cycle

---
+++ src/main/kotlin/com/goldy1992/leetcode/_141_linked_list_cycle/ListNode.kt
@@ -0,0 +1,5 @@
+package com.goldy1992.leetcode._141_linked_list_cycle
+
+class ListNode(var `val`: Int) {
+     var next: ListNode? = null
+}
---
+++ src/main/kotlin/com/goldy1992/leetcode/_141_linked_list_cycle/Solution.kt
@@ -0,0 +1,16 @@
+package com.goldy1992.leetcode._141_linked_list_cycle
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        return true
+    }
+}
