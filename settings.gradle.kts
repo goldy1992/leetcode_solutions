7f0a230c593c09aa0a668a08d6093cb323504246
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2024-05-02 03:56:14 +0100

    Gradle 8.7, kotlin 1.9.23

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
 }
+
 rootProject.name = "leetcode"
 
